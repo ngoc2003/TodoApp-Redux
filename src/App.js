1be1f4d4e0ee23f7cb4f155144268953c7dc61a7
buithuyngoc <buithuyngoc1@gmail.com>
2023-12-05 16:08:05 +0700

    feat: update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const { Title } = Typography;
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-800">
-      <div className="overflow-hidden max-w-[800px] bg-white  xs:rounded-md h-screen xs:h-auto p-5 pr-0  shadow-lg gap-x-10 w-full s:flex ">
+      <div className="overflow-hidden max-w-[800px] bg-white xs:rounded-md h-screen xs:h-auto p-5 m-5  shadow-lg gap-x-10 w-full s:flex ">
         <div className="flex flex-col flex-1 mt-5 xs:mt-0 ">
           <Title className="text-center">TODO APP </Title>
           <InputTodo />
